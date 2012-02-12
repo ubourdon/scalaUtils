6e29f0ee3a455efb22a3f7ea83ce4ceb47d21f11
ubourdon <bourdon.ugo@gmail.com>
2012-02-13 00:23:13 +0100

    add: awaitility uses rather than Thread.sleep

--- actor/build.sbt
+++ actor/build.sbt
@@ -1,3 +1,5 @@
 libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
 
 libraryDependencies += "org.slf4j" % "slf4j-api" % "1.6.4"
+
+libraryDependencies += "com.jayway.awaitility" % "awaitility-scala" % "1.3.3"
--- actor/src/test/scala/fr/scala/util/actor/ActorTestingTest.scala
+++ actor/src/test/scala/fr/scala/util/actor/ActorTestingTest.scala
@@ -3,6 +3,8 @@ package fr.scala.util.actor
 import akka.actor.{Actor, ActorRef}
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
+import com.jayway.awaitility.Awaitility._
+import com.jayway.awaitility.scala.AwaitilitySupport
 
 
 /**
@@ -10,9 +12,9 @@ import org.scalatest.matchers.ShouldMatchers
  * Date: 12/02/12
  */
 
-class ActorTestingTest extends FunSuite with ShouldMatchers {
+class ActorTestingTest extends FunSuite with ShouldMatchers with AwaitilitySupport {
 
-	test("simple actor testing with string message") {
+	test( "simple actor testing with string message" ) {
 		val actorSpy: ActorSpy[String] = new ActorSpy[String]
 		val testDoubleDependency: ActorRef = Actor.actorOf( new ActorStub(actorSpy) ).start()		
 		
@@ -23,22 +25,20 @@ class ActorTestingTest extends FunSuite with ShouldMatchers {
 		Thread.sleep( 100 )
 
 		actorSpy.get should be ( "salut toto" )
-
-		/*val actorSpy: ActorSpy[InstallUpdateFail] = new ActorSpy[InstallUpdateFail]
-		testDoubleRouter = Actor.actorOf(new ActorStub(actorSpy)).start()
-
-		val updater: Updater = mock(classOf[Updater])
-		when(updater.installUpdate(Matchers.any[Update])).thenThrow(new RuntimeException())
-
-		val expectedUpdate: Update = new UpdateBuilder().id(1).toUpdate
-
-		updaterActor = Actor.actorOf(new UpdaterActor(updater)).start()
-		updaterActor ! UpdaterStart(testDoubleRouter)
-
-		updaterActor ! InstallUpdate(expectedUpdate)
-
-		Thread.sleep(100)
-
-		actorSpy.get.update.id should be(expectedUpdate.id)*/
 	}
+	
+	test( "simple actor testing with awaitility support rather than Thread.sleep" ) {
+		val actorSpy: ActorSpy[String] = new ActorSpy[String]
+		val testDoubleDependency: ActorRef = Actor.actorOf( new ActorStub(actorSpy) ).start()
+
+		val underTestActor: ActorRef = Actor.actorOf( new TestingActor( testDoubleDependency ) ).start()
+
+		underTestActor ! "toto"
+
+		await until { receiveMsg( actorSpy ) }
+
+		actorSpy.get should be ( "salut toto" )
+	}
+
+	def receiveMsg( spy: ActorSpy[String] ): Boolean = spy.get() != null
 }
